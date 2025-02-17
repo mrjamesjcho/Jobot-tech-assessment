d919caf234683ed6b2f756bda5fa205fe00a4850
James Cho <mrjamesjcho@gmail.com>
2025-02-16 20:54:20 -0800

    add server

---
+++ src/server/graph/resolver.go
@@ -0,0 +1,7 @@
+package graph
+
+import "github.com/mrjamesjcho/jobot-assessment/src/server/graph/model"
+
+type Resolver struct {
+	ping *model.Ping
+}
---
+++ src/server/server.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"slices"
+	"time"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/lru"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	"github.com/gorilla/websocket"
+	"github.com/mrjamesjcho/jobot-assessment/src/server/graph"
+	"github.com/rs/cors"
+	"github.com/vektah/gqlparser/v2/ast"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	router := chi.NewRouter()
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:8080", "http://localhost:3001"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	srv.AddTransport(transport.Websocket{
+		KeepAlivePingInterval: 10 * time.Second,
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				origin := r.Header.Get("Origin")
+				if origin == "" || origin == r.Header.Get("Host") {
+					return true
+				}
+				return slices.Contains([]string{"http://localhost:3001", "http://localhost:3000"}, origin)
+			},
+		},
+	})
+	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.GET{})
+	srv.AddTransport(transport.POST{})
+
+	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
+
+	srv.Use(extension.Introspection{})
+	srv.Use(extension.AutomaticPersistedQuery{
+		Cache: lru.New[string](100),
+	})
+
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
